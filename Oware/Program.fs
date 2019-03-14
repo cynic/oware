5b07c0f7ec2655889c1740c76f3b4487a20d8ab6
Sinawo Mngxuma <g16M4204@ict.ru.ac.za>
2019-03-14 14:34:33 +0200

    adited comments

--- Oware/Program.fs
+++ Oware/Program.fs
@@ -4,14 +4,34 @@ type StartingPosition =
     | South
     | North
 
+
+    //getSeeds, which accepts a house number and a board,
+    //and returns the number of
+    //seeds in the specified house
 let getSeeds n board = failwith "Not implemented"
 
+
+//useHouse, which accepts a house number and a board,
+//and makes a move using
+//that house.
 let useHouse n board = failwith "Not implemented"
 
+//start, which accepts a StartingPosition and returns 
+//an initialized game where the person
+//in the StartingPosition starts the game
 let start position = failwith "Not implemented"
 
+
+//score, which accepts a board and gives back a 
+//tuple of (southScore , northScore)
 let score board = failwith "Not implemented"
 
+
+//gameState, which accepts a board and gives back 
+//a string that tells us about the
+//state of the game. Valid strings are
+//“South’s turn”, “North’s turn”, “Game ended in a draw”,
+//“South won”, and “North won”.
 let gameState board = failwith "Not implemented"
 
 [<EntryPoint>]
