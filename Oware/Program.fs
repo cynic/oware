52bf9be122783d2dcce3c9dab2dcd08abb9d5e61
Sinawo Mngxuma <g16M4204@ict.ru.ac.za>
2019-03-14 17:06:54 +0200

    team working in the practical

--- Oware/Program.fs
+++ Oware/Program.fs
@@ -3,7 +3,20 @@
 type StartingPosition =
     | South
     | North
+ type Player=
+      {
+       houses: int*int*int*int*int*int
+       capturedSeeds: int 
+      }
 
+ type board =
+      {
+           state : string
+           northPlayer : Player
+           southPlayer : Player
+           
+      }
+                
 
     //getSeeds, which accepts a house number and a board,
     //and returns the number of
@@ -15,13 +28,25 @@ let getSeeds n board = failwith "Not implemented"
 //and makes a move using
 //that house.
 let useHouse n board = failwith "Not implemented"
-
+      
+            
+//-------------------------------------------------
 //start, which accepts a StartingPosition and returns 
 //an initialized game where the person
 //in the StartingPosition starts the game
-let start position = failwith "Not implemented"
+let start position =
+      
+                match position = North  with
+                | true -> { state= "north" ; northPlayer = {(7,8,9,10,11,12) }
+                | _ -> North
 
-
+                //  //let var n = 
+                  //            match n <= 6 with
+                  //            |true -> position = South 
+                  //            | _ -> position = North
+                  //var  
+                
+                   
 //score, which accepts a board and gives back a 
 //tuple of (southScore , northScore)
 let score board = failwith "Not implemented"
