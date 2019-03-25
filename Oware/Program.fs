43cfe5821ff84fd161c6de51a643a2773b5a68fd
tandzakhoza <31188706+tandzakhoza@users.noreply.github.com>
2019-03-25 19:52:20 +0200

    updated getseeds and start code

--- Oware/Program.fs
+++ Oware/Program.fs
@@ -3,26 +3,47 @@
 type StartingPosition =
     | South
     | North
- type Player=
-      {
-       houses: int*int*int*int*int*int
-       capturedSeeds: int
-      }
 
- type board =
-      {
-           state : string
-           northPlayer : Player
-           southPlayer : Player
-           
-      }
+type Player={
+houses : int*int*int*int*int*int
+captured : int}
+
+type states=
+|Draw of string
+|Win of string
+|Northturn 
+|Southturn
+
+type Board={
+    state : states
+    northplayer : Player
+    southplayer : Player
+    }
                 
 
     //getSeeds, which accepts a house number and a board,
     //and returns the number of
     //seeds in the specified house
-let getSeeds n board = failwith "Not implemented"
-
+let getSeeds n board = // failwith "Not implemented"
+    match board.state with
+    |Northturn | Southturn -> 
+        let (a',b',c',d',e',f')= board.southplayer.houses 
+        let (a,b,c,d,e,f) = board.northplayer.houses in
+            match n with 
+            |1-> a
+            |2->b
+            |3->c
+            |4->d
+            |5->e
+            |6->f  
+            |7->a'
+            |8->b'
+            |9->c'
+            |10->d'
+            |11->e'
+            |12->f'
+            |_-> failwith "Not implemented"
+    |_-> failwith "Not implemented"
 
 //useHouse, which accepts a house number and a board,
 //and makes a move using
@@ -35,11 +56,10 @@ let useHouse n board = failwith "Not implemented"
 //an initialized game where the person
 //in the StartingPosition starts the game
 let start position =
-      
-                match position = North  with
-                //| true -> { state= "north" ; northPlayer ={(7,8,9,10,11,12);0};southPlayer = _}
-                | true -> { state= "north" ; northPlayer = {(4,4,4,4,4,4)};southPlayer={(4,4,4,4,4,4)}
-                | _ ->  { state= "south" ;_; southPlayer ={(1,2,3,4,5,6); 0 }}
+      let reco={houses=(4,4,4,4,4,4);captured=0}
+    match position with
+    |North -> {state = Northturn ; northplayer = reco;southplayer=reco}
+    |South->  {state = Southturn ; northplayer = reco;southplayer = reco}
 
               
                 
