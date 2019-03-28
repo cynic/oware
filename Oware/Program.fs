b96643d6b62cac68efe997d49b6577e8d59fb864
Sinawo Mngxuma <g16M4204@ict.ru.ac.za>
2019-03-28 17:24:02 +0200

    UseHouse

--- Oware/Program.fs
+++ Oware/Program.fs
@@ -1,6 +1,7 @@
 ﻿module Oware
 
 open System
+open System.Collections.Immutable
 
 type StartingPosition =
     | South
@@ -10,6 +11,7 @@ type StartingPosition =
       {
        houses: int*int*int*int*int*int
        capturedSeeds: int
+     
       }
 
  
@@ -18,7 +20,7 @@ type states=
 |Draw of string
 |Win of string
 |Northturn 
-|Southturn
+|Southturn 
 
 type Board = {
     state : states
@@ -34,11 +36,11 @@ type Board = {
 let getSeeds n board = // failwith "Not implemented"
 
     match board.state with
-    |Northturn | Southturn -> 
+    |Northturn | Southturn _ -> 
         let (a',b',c',d',e',f')= board.northplayer.houses 
         let (a,b,c,d,e,f) = board.southplayer.houses in
             match n with 
-            |1-> a
+            |1->a
             |2->b
             |3->c
             |4->d
@@ -57,31 +59,73 @@ let getSeeds n board = // failwith "Not implemented"
 //useHouse, which accepts a house number and a board,
 //and makes a move using
 //that house.
-let useHouse n board = // failwith "Not implemented"
-         let (a, b , c, d, e, f ) = board.southplayer.houses  //(4,4,4,4,4,4) thats a b c d e f   right (4,4,4,4,0,5,    5,5,5,4,4,4
-         let (a',b',c',d',e', f')= board.northplayer.houses 
-         //{state = Northturn ; northplayer = reco;southplayer=reco}
-         //{houses=(4,4,4,4,4,4);capturedSeeds=0} this must be southplayer type
-        // let k = board.southplayer in
-         match n with 
-         | 1 ->let v =  {board with southplayer = {houses = (a-4, b+1,c+1,d+1,e+1,f) ; capturedSeeds = 0;};northplayer = {houses = (a',b',c',d',e',f') ; capturedSeeds = 0}} in
-                          v
-         | 5 -> let v =  { board with southplayer = {houses = (a,b,c,d,e-4,f+1) ; capturedSeeds = 0} ;northplayer = {houses = (a'+1,b'+1,c'+1,d',e',f') ; capturedSeeds = 0}} in
-                          v                  
-          
-         | _ ->  failwith "Not implemented"
 
-            
-//-------------------------------------------------
+let nextHouse n =
+          match n  with 
+          |12 -> 1
+          | _ -> n+1
+
+let setSeeds n num board =
+     let (a',b',c',d',e',f') = board.northplayer.houses 
+     let (a ,b ,c ,d ,e ,f ) = board.southplayer.houses in
+         match n with
+         |1-> {board with southplayer={board.southplayer with houses=(num,b,c,d,e,f)}}
+         |2->{board with southplayer={board.southplayer with houses=(a,num,c,d,e,f)}}
+         |3->{board with southplayer={board.southplayer with houses=(a,b,num,d,e,f)}}
+         |4->{board with southplayer={board.southplayer with houses=(a,b,c,num,e,f)}}
+         |5->{board with southplayer={board.southplayer with houses=(a,b,c,d,num,f)}}
+         |6->{board with southplayer={board.southplayer with houses=(a,b,c,d,e,num)}}
+         |7->{board with northplayer={board.northplayer with houses=(num,b',c',d',e',f')}}
+         |8->{board with northplayer={board.northplayer with houses=(a',num,c',d',e',f')}}
+         |9->{board with northplayer={board.northplayer with houses=(a',b',num,d',e',f')}}
+         |10->{board with northplayer={board.northplayer with houses=(a',b',c',num,e',f')}}
+         |11->{board with northplayer={board.northplayer with houses=(a',b',c',d',num,f')}}
+         |12->{board with northplayer={board.northplayer with houses=(a',b',c',d',e',num)}}
+         |_-> failwith "Not implemented"
+
+ 
+
+
+
+let useHouse n board = // failwith "Not implemented"
+         let (a',b',c',d',e',f') = board.northplayer.houses 
+         let (a ,b ,c ,d ,e ,f ) = board.southplayer.houses in
+         let numSeeds = getSeeds n board
+         let newboard = setSeeds n 0 board
+
+
+        
+         let rec updateBoard currentHouse nSeeds newboard=
+              match nSeeds>0 with 
+              | true -> 
+                let nxtHouse = nextHouse n
+                let numSeeds = getSeeds nxtHouse newboard
+                //updateBoard (nextHouse n) (getSeeds (nextHouse n) board) in
+                let board = setSeeds nxtHouse (numSeeds+1) newboard
+                updateBoard nxtHouse (numSeeds-1) board
+              | _ -> newboard
+
+         updateBoard n numSeeds newboard
+
+
+
+        
+
+        
+         
+
+
+           
+//-----------------------------------------------
 //start, which accepts a StartingPosition and returns 
 //an initialized game where the person
 //in the StartingPosition starts the game
 let start position =
   
-    let reco={houses=(4,4,4,4,4,4);capturedSeeds=0}
+    let reco={houses=(4,4,4,4,4,4); capturedSeeds = 0}
     match position with
     |North -> {state = Northturn ; northplayer = reco;southplayer=reco}
-    |South->  {state = Southturn ; northplayer = reco;southplayer = reco}
+    |South->  {state = Southturn  ; northplayer = reco;southplayer = reco}
 
 
               
@@ -104,53 +148,4 @@ let main _ =
     printfn "Hello from F#!"
     0 // return an integer exit code
   
-  (*let useHouse n board = // failwith "Not implemented"
-         let (a, b , c, d, e, f )= board.southplayer.houses  //(4,4,4,4,4,4) thats a b c d e f   right
-         let (a',b',c',d',e', f')= board.northplayer.houses 
-         //{state = Northturn ; northplayer = reco;southplayer=reco}
-         //{houses=(4,4,4,4,4,4);capturedSeeds=0} this must be southplayer type
-         let k = board.southplayer in
-         match n with 
-         | 1 -> let CorrectRecordPlayer  = {k with  houses = (a-4, b+1,c+1,d+1,e+1,f)} in
-                                                     CorrectRecordPlayer
-         //| 2 -> let CorrectRecordPlayer  = {k with  houses = (a, b-4,c+1,d+1,e+1,f+1)} in
-         //                                            CorrectRecordPlayer
-         //| 3 -> let CorrectRecordPlayer  = {k with  houses = (a, b, c-4,d+1,e+1,f+1)} in
-         //                                            CorrectRecordPlayer
-         //| 4 ->let CorrectRecordPlayer  = {k with  houses = (a-4, b+1,c+1,d+1,e+1,f)} in
-         //                                            CorrectRecordPlayer
-         //| 5 ->let CorrectRecordPlayer  = {k with  houses = (a-4, b+1,c+1,d+1,e+1,f)} in
-         //                                            CorrectRecordPlayer
-         //| 6 -> let CorrectRecordPlayer  = {k with  houses = (a-4, b+1,c+1,d+1,e+1,f)} in
-         //                                            CorrectRecordPlayer
-         //| 7 -> let CorrectRecordPlayer  = {k with  houses = (a-4, b+1,c+1,d+1,e+1,f)} in
-         //                                            CorrectRecordPlayer
-         //| 8 -> let CorrectRecordPlayer  = {k with  houses = (a-4, b+1,c+1,d+1,e+1,f)} in
-         //                                            CorrectRecordPlayer
-         //| 9 -> let CorrectRecordPlayer  = {k with  houses = (a-4, b+1,c+1,d+1,e+1,f)} in
-         //                                            CorrectRecordPlayer
-         //| 10 -> let CorrectRecordPlayer  = {k with  houses = (a-4, b+1,c+1,d+1,e+1,f)} in
-         //                                            CorrectRecordPlayer
-         //| 11-> let CorrectRecordPlayer  = {k with  houses = (a-4, b+1,c+1,d+1,e+1,f)} in
-         //                                            CorrectRecordPlayer
-         //| 12 -> let CorrectRecordPlayer  = {k with  houses = (a-4, b+1,c+1,d+1,e+1,f)} in
-         //                                            CorrectRecordPlayer
-
-
-         
-         //let v =  {board with northplayer = {houses = (a-4, b+1,c+1,d+1,e+1,f) ; capturedSeeds = 0;}} in
-         //                 v
-                            
-            //|2->{k with houses = (a-4, b+1,c+1,d+1,e+1,f ) }
-            //|3->{k with houses = (a-4, b+1,c+1,d+1,e+1,f ) }
-            //|4->{k with houses = (a-4, b+1,c+1,d+1,e+1,f ) }
-            //|5->{k with houses = (a-4, b+1,c+1,d+1,e+1,f ) }
-            //|6->{k with houses = (a-4, b+1,c+1,d+1,e+1,f ) }
-            //|7->{k with houses = (a-4, b+1,c+1,d+1,e+1,f ) }
-            //|8->{k with houses = (a-4, b+1,c+1,d+1,e+1,f ) }
-            //|9->{k with houses = (a-4, b+1,c+1,d+1,e+1,f ) }
-            //|10->{k with houses = (a-4, b+1,c+1,d+1,e+1,f ) }
-            //|11->{k with houses = (a-4, b+1,c+1,d+1,e+1,f ) }
-            //|12->{k with houses = (a-4, b+1,c+1,d+1,e+1,f ) }
-         | _ ->  failwith "Not implemented"
-*)
+  
