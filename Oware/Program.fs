168bf45e4478ebbb228a18ba54c4e02c8c31532e
Marc <g18m6731@campus.ru.ac.za>
2020-04-09 16:20:22 +0200

    Player and board Record

--- Oware/Program.fs
+++ Oware/Program.fs
@@ -4,9 +4,19 @@ type StartingPosition =
     | South
     | North
 
+type player = {
+    house : int*int*int*int*int*int
+    capturedseeds : int}
+
+type Board={
+    northplayer : player
+    southplayer : player
+    }
+
 let getSeeds n board = failwith "Not implemented"
 
-let useHouse n board = failwith "Not implemented"
+let useHouse n board = 
+    
 
 let start position = failwith "Not implemented"
 
