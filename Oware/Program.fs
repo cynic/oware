231029d421fe4fbfbe12cfd2ec5cf207bb15b9e2
MphoSaba <g17s0815.campus.ru.ac.za>
2020-04-15 14:45:24 +0200

    Mpho with start and getSeeds

--- Oware/Program.fs
+++ Oware/Program.fs
@@ -13,12 +13,30 @@ type Board={
     southplayer : player
     }
 
-let getSeeds n board = failwith "Not implemented"
+let getSeeds n board = 
+    (a,b,c,d,e,f,A,B,C,D,E,F) = board
+    match n with
+    | 1 -> a
+    | 2 -> b
+    | 3 -> c
+    | 4 -> d
+    | 5 -> e
+    | 6 -> f 
+    | 7 -> A
+    | 8 -> B
+    | 9 -> C
+    | 10 -> D
+    | 11 -> E
+    | 12 -> F
+    | _ -> failwith "Invalid number on board"
 
 let useHouse n board = 
-    
 
-let start position = failwith "Not implemented"
+let start position = 
+    match position with
+    | North -> North
+    | South -> South 
+    | _ -> failwith "Invalid starting position"
 
 let score board = failwith "Not implemented"
 
