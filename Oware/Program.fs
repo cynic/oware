872a2b60878a853f11a158cc9b76d43d25e9b3f3
Marc <g18m6731@campus.ru.ac.za>
2020-04-21 11:17:37 +0200

    Fixed erros, add valid_house_selected function and additional member in board record to check which players turn it is

--- Oware/Program.fs
+++ Oware/Program.fs
@@ -5,14 +5,15 @@ type StartingPosition =
     | North
 
 type Board = {
-    houses: int*int*int*int*int*int*int*int*int*int*int*int
+    houses: int*int*int*int*int*int*int*int*int*int*int*int  //Houses 1-12  Houses 1-6 belong to South player and Houses 7-12 belong to North player
     score : int*int // (south, north)
+    turn : bool //checks whos turn it is true = South's turn whereas false = North's turn
     }
 
 let getSeeds n board = 
     let (h1,h2,h3,h4,h5,h6,h7,h8,h9,h10,h11,h12) = board.houses
     match n with
-    | 1 -> h1| 2 -> h2 | 3 -> h3 | 4 -> h4 | 5 -> h5 | 6 -> h6 |
+    | 1 -> h1| 2 -> h2 | 3 -> h3 | 4 -> h4 | 5 -> h5 | 6 -> h6 
     | 7 -> h7 | 8 -> h8 | 9 -> h9 | 10 -> h10 | 11 -> h11 | 12 -> h12 | _ -> failwith "Invalid number on board"
 
 //used with useHouse
@@ -33,13 +34,23 @@ let plant_or_harvest n board house_num = //n= number of seeds meant to be in the
     |12 -> {board with houses = (h1,h2,h3,h4,h5,h6,h7,h8,h9,h10,h11,n)} 
     |_ -> failwith "Invalid house number"
 
-let harvest house_num board num_seeds = //harvest seeds
-    let (h1,h2,h3,h4,h5,h6,h7,h8,h9,h10,h11,h12) = board.houses
+let harvest board num_seeds = //harvest seeds
     let (South_seeds, North_seeds) = board.score 
     match gameState board with
     |"South's turn" -> {board with score = (South_seeds+num_seeds, North_seeds)}
     |"Norths's turn" -> {board with score = (South_seeds, North_seeds+num_seeds)}
-    
+    | _ -> failwith "Game has ended or error somewhere"
+
+let valid_house_selected house_num board = //checkes if the player selected a valid house belonging to them and the house has seeds in it
+    match gameState board with
+    |"South's turn" -> match house_num>=1 && house_num <=6 && getSeeds house_num board > 0  with
+                       | true -> true
+                       | false -> false
+    |"Norths's turn" -> match house_num>=7 && house_num <=12 && getSeeds house_num board > 0 with
+                        | true -> true
+                        | false -> false
+    | _ -> false
+
 
 let useHouse n board = 
     let numseeds = getSeeds n board
@@ -56,13 +67,16 @@ let useHouse n board =
         | false -> //work back through planted houses
             match getSeeds (house_num-1) board with //does the last house we planted in contain two or three seeds?
             | 3 -> match (house_num-1) with //loop through houses if house num = 1 then next house must be house 12
-                   |1 -> plantseeds seeds (plant_or_harvest 0 (harvest (house_num-1) updateboard 3) (house_num-1)) 12)
-                   |_ -> plantseeds seeds (plant_or_harvest 0 (harvest (house_num-1) updateboard 3) (house_num-1)) (house_num-1) 
+                   |1 -> plantseeds seeds (plant_or_harvest 0 (harvest updateboard 3) (house_num-1)) 12
+                   |_ -> plantseeds seeds (plant_or_harvest 0 (harvest updateboard 3) (house_num-1)) (house_num-1) 
             | 2 -> match (house_num-1) with
-                    |1 -> plantseeds seeds (plant_or_harvest 0 (harvest (house_num-1) updateboard 2) (house_num-1)) 12 
-                    |_ -> plantseeds seeds (plant_or_harvest 0 (harvest (house_num-1) updateboard 3) (house_num-1)) (house_num-1)  
+                    |1 -> plantseeds seeds (plant_or_harvest 0 (harvest updateboard 2) (house_num-1)) 12 
+                    |_ -> plantseeds seeds (plant_or_harvest 0 (harvest updateboard 3) (house_num-1)) (house_num-1)  
             | _ -> updateboard 
-    plantseeds(numseeds board n) 
+    match (valid_house_selected n board) with
+    | true -> plantseeds numseeds board n //start recursive function
+    | false -> failwith "Invalid house selected"
+
     
 
 
