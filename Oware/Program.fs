fd397cc9e4d2e3e240d7c143affad3703362acdc
saramaegoodes <g18g5043@campus.ru.ac.za>
2020-05-16 16:37:28 +0200

    Start function has been added

--- Oware/Program.fs
+++ Oware/Program.fs
@@ -4,11 +4,26 @@ type StartingPosition =
     | South
     | North
 
+type state =
+|Turn
+|SouthWon
+|NorthWon
+
+type BoardState = {
+player: StartingPosition
+board: (int*int*int*int*int*int*int*int*int*int*int*int)
+score: (int*int)
+gamestatus: state
+}
+
 let getSeeds n board = failwith "Not implemented"
 
 let useHouse n board = failwith "Not implemented"
 
-let start position = failwith "Not implemented"
+let start position = 
+    match position with
+    |South -> {player=position; score=(0,0); board=(4,4,4,4,4,4,4,4,4,4,4,4); gamestatus = Turn}
+    |North -> {player=position; score=(0,0); board=(4,4,4,4,4,4,4,4,4,4,4,4); gamestatus = Turn}
 
 let score board = failwith "Not implemented"
 
