dcd80389ca332f93ca476c6fd85a5bd872101b3e
Yusuf Motara <y.motara@ru.ac.za>
2021-02-28 22:25:28 +0200

    Changes for 2021

--- Oware/Program.fs
+++ Oware/Program.fs
@@ -1,20 +1,24 @@
 ﻿module Oware
 
 type StartingPosition =
-    | South
-    | North
+| South
+| North
 
-let getSeeds n board = failwith "Not implemented"
+let getSeeds n board =
+    failwith "Not implemented"
 
-let useHouse n board = failwith "Not implemented"
+let useHouse n board =
+    failwith "Not implemented"
 
-let start position = failwith "Not implemented"
+let start position =
+    failwith "Not implemented"
 
-let score board = failwith "Not implemented"
+let score board =
+    failwith "Not implemented"
 
-let gameState board = failwith "Not implemented"
+let gameState board =
+    failwith "Not implemented"
 
 [<EntryPoint>]
 let main _ =
-    printfn "Hello from F#!"
     0 // return an integer exit code
